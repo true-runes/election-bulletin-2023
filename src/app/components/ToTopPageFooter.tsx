d519e5d44e2c3570d10b2fa2f2fa4f067dcc341a
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-01 17:29:51 +0900

    feat: 🎸 トップページへ戻るコンポーネントを追加し、配置した

---
+++ src/app/components/ToTopPageFooter.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+export const ToTopPageFooter = () => {
+  return (
+    <div className="relative overflow-hidden">
+      <div className="text-center">
+        <h1 className="font-bold text-white pt-2 pb-6">
+          <Link
+            href="/"
+            passHref
+            style={{
+              textDecoration: 'underline',
+              textUnderlineOffset: '0.2em',
+            }}
+          >
+            <div>トップページへ</div>
+          </Link>
+        </h1>
+      </div>
+    </div>
+  )
+}
