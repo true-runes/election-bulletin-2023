1939f23f98e41168e5c6a6df1aa8ce798eac40ed
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-01 17:30:04 +0900

    refactor: 💡 tailwind で dark: 指定の不要な記述を削除した (#58)

--- src/app/cities/page.tsx
+++ src/app/cities/page.tsx
@@ -8,10 +8,10 @@ const Cities = () => {
     <div className="relative overflow-hidden">
       <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24">
         <div className="text-center">
-          <h1 className="text-5xl sm:text-5xl font-bold text-white dark:text-white py-2">
+          <h1 className="text-5xl sm:text-5xl font-bold text-white py-2">
             幻水総選挙2023
           </h1>
-          <h1 className="text-3xl sm:text-3xl font-bold text-white dark:text-white py-2">
+          <h1 className="text-3xl sm:text-3xl font-bold text-white py-2">
             街部門
           </h1>
         </div>
@@ -23,7 +23,7 @@ const Cities = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {cities.map((city: any, index: number) => (
             <div className="hoge" key={index}>
-              <p className="fuga">
+              <p className="fuga px-2">
                 {city.city}（{city.title}）
               </p>
             </div>
