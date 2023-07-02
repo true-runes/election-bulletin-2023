346679d3ee99c6679e515677fa68c8a1a2baefbb
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-02 20:30:55 +0900

    feat: 🎸 h2 タグを追加し、余白を調整した

--- src/app/cities/page.tsx
+++ src/app/cities/page.tsx
@@ -7,16 +7,19 @@ import { GensosenkyoFooter } from '@/app/components/GensosenkyoFooter'
 const Cities = () => {
   return (
     <div className="relative overflow-hidden">
-      <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24">
+      <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:pt-24 pb-10">
         <div className="text-center">
           <h1 className="text-5xl sm:text-5xl font-bold text-white py-2">
             幻水総選挙2023
           </h1>
-          <h1 className="text-3xl sm:text-3xl font-bold text-white py-2">
+          <h2 className="text-4xl sm:text-4xl font-bold text-white py-2">
+            投票対象リスト
+          </h2>
+          <h2 className="text-3xl sm:text-3xl font-bold text-white py-2">
             街部門
-          </h1>
+          </h2>
         </div>
-        <div className="mt-2">
+        <div className="mt-4">
           <hr />
         </div>
       </div>
@@ -31,7 +34,7 @@ const Cities = () => {
           ))}
         </div>
       </div>
-      <div className="my-6">
+      <div className="mt-10 mb-4">
         <hr />
       </div>
 
