de11f5133a3cc6ed30a9faebc1836c02155e61cf
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-10 08:41:50 +0900

    feat: 🎸 404ページを追加した (src/app/not-found.tsx)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
+const NotFound = () => {
+  return (
+    <div className="bg-white h-screen w-screen flex justify-center items-center">
+      <img src="/akiramenai_404.png" alt="404" width={500} height={500} />
+    </div>
+  )
+}
+
+export default NotFound
