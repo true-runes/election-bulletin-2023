fa2886658a85bdb38ca3b852f0636fcdafc2305a
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-10 10:41:31 +0900

    fix: 🐛 404ページが正しく表示されていなかったので修正した (#87)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   output: 'export',
   distDir: 'out',
+  images: {
+    // next/image を統一的に使いたいので SSG 向けに設定する
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,11 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
+
 const NotFound = () => {
   return (
     <div className="bg-white h-screen w-screen flex justify-center items-center">
-      <img src="/akiramenai_404.png" alt="404" width={500} height={500} />
+      <div>
+        <Image src="/akiramenai_404.png" alt="404" width={500} height={500} />
+      </div>
     </div>
   )
 }
