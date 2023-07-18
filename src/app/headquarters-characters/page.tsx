b9d33925e44c99f7c144a124b6c74dd86413cff5
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-19 04:03:33 +0900

    refactor: 💡 コンポーネントに切り出した

--- src/app/headquarters-characters/page.tsx
+++ src/app/headquarters-characters/page.tsx
@@ -2,9 +2,7 @@ import productToHeadquartersCharacters from '@/app/resource/headquartersCharacte
 
 import { ToTopPageFooter } from '@/app/components/ToTopPageFooter'
 import { GensosenkyoFooter } from '@/app/components/GensosenkyoFooter'
-import { HelloWorld } from '@/app/components/HelloWorld'
-import { SvgAccordionUp } from '@/app/components/SvgAccordionUp'
-import { SvgAccordionDown } from '@/app/components/SvgAccordionDown'
+import { CharacterCard } from '@/app/components/CharacterCard'
 
 export const metadata = {
   title: '本拠地施設キャラ部門',
@@ -28,7 +26,6 @@ const Cities = () => {
         <div className="my-4">
           <hr />
         </div>
-        <HelloWorld />
       </div>
 
       <div className="text-center">
@@ -42,34 +39,11 @@ const Cities = () => {
                 </h1>
                 {headquartersCharacters.map(
                   (headquartersCharacter: any, index: number) => (
-                    <div className="city-and-title px-6 py-1" key={index}>
-                      <div className="flex flex-col bg-white border shadow-sm rounded-xl p-2 md:p-3 dark:bg-gray-900 dark:border-gray-700 dark:shadow-slate-700/[.7] dark:text-white my-2">
-                        <div className="hs-accordion-group">
-                          <div
-                            className="hs-accordion active ml-2"
-                            id="hs-basic-with-arrow-heading-one"
-                          >
-                            <button
-                              className="hs-accordion-toggle hs-accordion-active:text-blue-600 group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
-                              aria-controls="hs-basic-with-arrow-collapse-one"
-                            >
-                              <SvgAccordionUp />
-                              <SvgAccordionDown />
-                              <span> {headquartersCharacter}</span>
-                            </button>
-                            <div
-                              id="hs-basic-with-arrow-collapse-one"
-                              className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
-                              aria-labelledby="hs-basic-with-arrow-heading-one"
-                            >
-                              <div className="text-left text-gray-800 dark:text-gray-200 pb-4">
-                                本拠地で働く＊＊。いつも忙しそうにしている。好きな食べ物はメロン。
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                    <CharacterCard
+                      key={index}
+                      index={index}
+                      headquartersCharacter={headquartersCharacter}
+                    />
                   )
                 )}
               </div>
