500f75161b093e55b4795f5eb068121c946c1f5a
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-19 19:08:37 +0900

    feat: 🎸 街部門もカード化した

--- src/app/cities/page.tsx
+++ src/app/cities/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import cities from '@/app/resource/cities.json'
 import { ToTopPageFooter } from '../components/ToTopPageFooter'
 import { GensosenkyoFooter } from '@/app/components/GensosenkyoFooter'
@@ -12,10 +13,14 @@ const Cities = () => {
       <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:pt-24 pb-10">
         <div className="text-center">
           <h1 className="text-5xl sm:text-5xl font-bold text-white py-2">
-            幻水総選挙2023
+            <Link href="/" passHref>
+              幻水総選挙2023
+            </Link>
           </h1>
           <h2 className="text-4xl sm:text-4xl font-bold text-white py-2">
-            投票対象リスト
+            <Link href="/" passHref>
+              投票対象リスト
+            </Link>
           </h2>
           <h2 className="text-3xl sm:text-3xl font-bold text-white py-2">
             街部門
@@ -27,16 +32,17 @@ const Cities = () => {
       </div>
       <div className="text-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {/* key である product を h1 とし、values を並べる */}
           {Object.entries(cities).map(([city, characters]) => (
             <div className="cities-and-titles" key={city}>
-              <h1 className="city-and-title text-2xl px-6 my-4 underline underline-offset-8">
+              <h1 className="city-and-title text-3xl px-6 my-4 underline underline-offset-8">
                 {city}
               </h1>
               {characters.map((character: any, index: number) => (
-                <p className="city-and-title px-6 py-1" key={index}>
-                  {character}
-                </p>
+                <div className="city-and-title px-6 py-1" key={index}>
+                  <div className="flex flex-col border shadow-sm rounded-xl p-2 md:p-3 bg-gray-900 border-gray-700 shadow-slate-700/[.7] text-white my-2">
+                    {character}
+                  </div>
+                </div>
               ))}
             </div>
           ))}
--- src/app/headquarters-characters/page.tsx
+++ src/app/headquarters-characters/page.tsx
@@ -1,5 +1,5 @@
+import Link from 'next/link'
 import productToHeadquartersCharacters from '@/app/resource/headquartersCharacters.json'
-
 import { ToTopPageFooter } from '@/app/components/ToTopPageFooter'
 import { GensosenkyoFooter } from '@/app/components/GensosenkyoFooter'
 
@@ -13,10 +13,14 @@ const Cities = () => {
       <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:pt-24 pb-4">
         <div className="text-center">
           <h1 className="text-5xl sm:text-5xl font-bold text-white py-2">
-            幻水総選挙2023
+            <Link href="/" passHref>
+              幻水総選挙2023
+            </Link>
           </h1>
           <h2 className="text-4xl sm:text-4xl font-bold text-white py-2">
-            投票対象リスト
+            <Link href="/" passHref>
+              投票対象リスト
+            </Link>
           </h2>
           <h2 className="text-3xl sm:text-3xl font-bold text-white py-2">
             本拠地施設キャラ部門
@@ -29,17 +33,16 @@ const Cities = () => {
 
       <div className="text-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {/* key である product を h1 とし、values を並べる */}
           {Object.entries(productToHeadquartersCharacters).map(
             ([product, headquartersCharacters]) => (
               <div className="cities-and-titles" key={product}>
-                <h1 className="city-and-title text-2xl px-6 py-4 underline underline-offset-8">
+                <h1 className="city-and-title text-3xl px-6 py-4 underline underline-offset-8">
                   {product}
                 </h1>
                 {headquartersCharacters.map(
                   (headquartersCharacter: any, index: number) => (
                     <div className="city-and-title px-6 py-1" key={index}>
-                      <div className="flex flex-col bg-white border shadow-sm rounded-xl p-2 md:p-3 dark:bg-gray-900 dark:border-gray-700 dark:shadow-slate-700/[.7] dark:text-white my-2">
+                      <div className="flex flex-col border shadow-sm rounded-xl p-2 md:p-3 bg-gray-900 border-gray-700 shadow-slate-700/[.7] text-white my-2">
                         {headquartersCharacter}
                       </div>
                     </div>
